736d5195bcc4ae62389d6ae908bc18cb7aa7f25c
FranAGC <frcontreras.78@gmail.com>
2022-10-01 01:19:54 -0600

    ejercicio 11 terminado, cors configurado

--- ej11/ej11.js
+++ ej11/ej11.js
@@ -9,12 +9,12 @@ app.use(cors())
 
 app.listen(puerto, function () {
     console.log(`Servidor corriendo en el puerto ${puerto}`);
-    app.get('*',function(req,res){  
+    app.get('/',function(req,res){  
         res.redirect('http://127.0.0.1:5500/ej11/index.html')
     })
 });
 
-app.get('/', (req, res) => {
+app.get('/productos', (req, res) => {
     res.redirect('http://127.0.0.1:5500/ej11/productos.html')
 });
 
@@ -22,4 +22,20 @@ app.get('/home', cors(), (req, res) => {
     res.json({
         h1: faker.internet.domainWord(), p1: faker.lorem.paragraphs(2), p2: faker.lorem.paragraphs(1), p3: faker.lorem.paragraphs(2)
     })
+});
+
+
+app.get('/listaprods', cors(), (req, res) => {
+    const productos = [];
+    const {size} = req.query;
+    const limit = size || 10;
+    for(let i=0; i < limit; i++){
+        productos.push( {
+            id: i+1,
+            nombre: faker.commerce.product(),
+            precio: faker.commerce.price(),
+            lugar: faker.address.city(),
+        })
+    }
+    res.json(productos);
 });
--- ej11/index.js
+++ ej11/index.js
@@ -1,15 +1,14 @@
 
 
 
-//Obtener generos
+//Obtener home
 async function obtenerHome(){
     var home = await (await fetch(`http://127.0.0.1:3000/home`)).json();
     console.log("Datos Home:", home);
     agregarHome(home);
 }
 
-
-//Agregar generos al select
+//Agregar datos al home
 function agregarHome(obj){
     let autogen  = document.getElementById('ej12index');
 
@@ -20,4 +19,23 @@ function agregarHome(obj){
                             <p>${obj.p3}</p>`
 }
 
-obtenerHome();
+
+//Obtener Productos
+async function obtenerProductos(){
+    var home = await (await fetch(`http://127.0.0.1:3000/listaprods`)).json();
+    console.log("Lista productos:", home);
+    agregarProductos(home);
+}
+
+//Agregar productos
+function agregarProductos(obj){
+    let tabla  = document.getElementById('lproductos');
+     
+    for (var i = 0; i < 10; i++) {
+
+        tabla.innerHTML += `
+                    <tr><td>${obj[i].nombre}</td>
+                    <td>${obj[i].precio}</td>
+                    <td>${obj[i].lugar}</td></tr>`;
+    }
+}
